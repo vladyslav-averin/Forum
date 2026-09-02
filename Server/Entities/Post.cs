779c2d6a39231c8589cb241ec2cca28cedbb9a85
Vladyslav <contact@averin.dk>
2026-09-03 01:13:14 +0200

    Add domain entities

---
+++ Server/Entities/Post.cs
@@ -0,0 +1,10 @@
+namespace Entities;
+
+public class Post
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = "";
+    public string Body { get; set; } = "";
+    public int UserId { get; set; }
+    
+}
